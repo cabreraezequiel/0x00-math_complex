56069de08e618de1d6b24606b8f52db5430ecfca
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-17 10:10:28 -0700

    0-complex

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -9,7 +9,7 @@ void display_complex_number(complex c)
 {
 	if (c.re != 0)
 	{
-		printf("%g", c.re);
+		printf("%G", c.re);
 		if (c.im != 0)
 		{
 			printf(" + ");
@@ -17,7 +17,7 @@ void display_complex_number(complex c)
 	}
 	if (c.im != 0)
 	{
-		printf("%g", c.im);
+		printf("%G", c.im);
 	}
 	if (c.re != 0 || c.im != 0)
 	{
