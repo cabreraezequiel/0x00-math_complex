5f7b1713bead0f55dcf05aa96fc6c27d3bb7d3aa
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-18 08:42:19 -0700

    Fixed 0-display,c

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -9,30 +9,22 @@ void display_complex_number(complex c)
 {
 	if (c.re == 0)
 	{
-		printf("0");
-		if (c.im == 1)
-			printf(" + i");
-		if (c.im == -1)
-			printf(" - i");
-		if (c.im < -1)
-			printf(" - %.9gi", -(c.im));
-		if (c.im > 1)
-			printf(" + %.9gi", c.im);
+		if (c.im < 0)
+			printf("- %.9gi\n", -(c.im));
+		else if (c.im == 0)
+			printf("0\n");
+		else
+			printf("%.9gi\n", c.im);
 	}
 	else
 	{
-		if (c.re > 0)
-			printf("%.9g", c.re);
-		if (c.re < 0)
-			printf("- %.9g", -(c.re));
 		if (c.im == 1)
-			printf(" + i");
+			printf("%.9g + i\n", c.re);
 		if (c.im == -1)
-			printf(" - i");
+			printf("%.9g - i\n", c.re);
 		if (c.im < -1)
-			printf(" - %.9gi", -(c.im));
-		if (c.im > 1)
-			printf(" + %.9gi", c.im);
+			printf("%.9g - %.9gi\n", c.re, -(c.im));
+		else
+			printf("%.9g + %.9gi\n", c.re, c.im);
 	}
-	printf("\n");
 }
