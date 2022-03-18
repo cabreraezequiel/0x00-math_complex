cf927047fd1fa94efba141a143c04630769d6717
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-18 08:34:46 -0700

    Fixed 0-display,c

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -9,12 +9,15 @@ void display_complex_number(complex c)
 {
 	if (c.re == 0)
 	{
-		if (c.im == 0)
-			printf("0\n");
-		if (c.im < 0)
-			printf("- %.9gi", -(c.im));
-		if (c.im > 0)
-			printf("%.9gi", c.im);
+		printf("0");
+		if (c.im == 1)
+			printf(" + i");
+		if (c.im == -1)
+			printf(" - i");
+		if (c.im < -1)
+			printf(" - %.9gi", -(c.im));
+		if (c.im > 1)
+			printf(" + %.9gi", c.im);
 	}
 	else
 	{
