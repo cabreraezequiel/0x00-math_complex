ec3fbb94def58bf49acd735217ea9eef3d7d4104
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-18 06:43:42 -0700

    0-display.c

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -7,35 +7,26 @@
 
 void display_complex_number(complex c)
 {
-	if (c.re != 0)
+	if (c.re == 0)
 	{
-		if (c.re < 0)
-			printf(" - %.9g", -(c.re));
-		else
-			printf("%.9g", c.re);
-		if (c.im > 0)
-		{
-			printf(" + ");
-		}
 		if (c.im == 0)
-		{
-			printf("\n");
-		}
+			printf("0\n");
+		if (c.im < 0)
+			printf("- %.9g\n", -(c.im));
+		if (c.im > 0)
+			printf("%.9g\n", c.im);
 	}
-	if (c.im > 1)
+	else
 	{
-		printf("%.9g", c.im);
-	}
-	if (c.im < 0)
-	{
-		printf(" - %.9g", -(c.im));
-	}
-	if (c.im != 0)
-	{
-		printf("i\n");
-	}
-	if (c.re == 0 && c.im == 0)
-	{
-		printf("0\n");
+		if (c.re > 0 && c.im > 0)
+			printf("%.9g + %.9g\n", c.re, c.im);
+		if (c.re < 0 && c.im > 0)
+			printf("- %.9g + %.9g\n", -c.re, c.im);
+		if (c.re > 0 && c.im < 0)
+			printf("%.9g - %.9g\n", c.re, -c.im);
+		if (c.re < 0 && c.im < 0)
+			printf("- %.9g - %.9g\n", -c.re, -c.im);
+		if (c.im == 0)
+			printf("%.9g\n", c.re);
 	}
 }
