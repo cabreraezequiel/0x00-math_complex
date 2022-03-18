80bfe845f4634806dd25ecdcceee23c3f244807b
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-18 09:25:51 -0700

    8-complex.c

--- 0x00-math_complex/8-complex.c
+++ 0x00-math_complex/8-complex.c
@@ -8,10 +8,8 @@
  * @c3: result
  */
 
-void complex_from_mod_arg (double m, double arg, complex *c3)
+void complex_from_mod_arg(double m, double arg, complex *c3)
 {
-	double PI = acos(-1.0);
-
-	c3->re = tan((arg * 180) / PI) * m;
-	c3->im = sin((arg * 180) / PI) * m;
+	c3->re = tan(arg) * m;
+	c3->im = sin(arg) * m;
 }
