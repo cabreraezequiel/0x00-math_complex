2ac82514073627ce6b764a050e7787d42ac32d18
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-18 09:09:22 -0700

    7-division.c

--- 0x00-math_complex/7-division.c
+++ 0x00-math_complex/7-division.c
@@ -11,6 +11,6 @@ void division(complex c1, complex c2, complex *c3)
 {
 	double a = c1.re, b = c1.im, c = c2.re, d = c2.im;
 
-	c3->re = (a * c + b * d) / (pow(c, 2) + pow(d, 2));
-	c3->im = (b * c - a * d) / (pow(c, 2) + pow(d, 2));
+	c3->re = (a * c + b * d) / (c * c + d * d);
+	c3->im = (b * c - a * d) / (c * c + d * d);
 }
