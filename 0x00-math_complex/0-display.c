79cebd3a4b48b95fe603c741e2f530cd63b3230f
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-18 08:47:51 -0700

    Another fix for 0-display.c

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -10,7 +10,7 @@ void display_complex_number(complex c)
 	if (c.re == 0)
 	{
 		if (c.im < 0)
-			printf("- %.9gi\n", -(c.im));
+			printf("- %.9gi\n", c.im * (-1));
 		else if (c.im == 0)
 			printf("0\n");
 		else
@@ -18,12 +18,14 @@ void display_complex_number(complex c)
 	}
 	else
 	{
-		if (c.im == 1)
-			printf("%.9g + i\n", c.re);
-		if (c.im == -1)
-			printf("%.9g - i\n", c.re);
 		if (c.im < -1)
-			printf("%.9g - %.9gi\n", c.re, -(c.im));
+			printf("%.9g - %.9gi\n", c.re, c.im * (-1));
+		else if (c.im == -1)
+			printf("%.9g - i\n", c.re);
+		else if (c.im == 0)
+			printf("%.9g\n", c.re);
+		else if (c.im == 1)
+			printf("%.9g + i\n", c.re);
 		else
 			printf("%.9g + %.9gi\n", c.re, c.im);
 	}
