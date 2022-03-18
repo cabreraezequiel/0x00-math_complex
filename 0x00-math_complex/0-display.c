e1b87110a859716f9b5086606c8320f1335a626d
Ezequiel Cabrera <98030236+cabreraezequiel@users.noreply.github.com>
2022-03-18 03:35:05 -0300

    Update 0-display.c

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -10,14 +10,10 @@ void display_complex_number(complex c)
 	if (c.re != 0)
 	{
 		printf("%g", c.re);
-		if (c.im != 0)
-		{
-			printf(" + ");
-		}
 	}
 	if (c.im != 0 && c.im != 1)
 	{
-		printf("%g", c.im);
+		printf("%+g", c.im);
 	}
 	if (c.im != 0)
 	{
