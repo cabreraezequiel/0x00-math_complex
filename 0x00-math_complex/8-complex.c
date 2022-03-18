1fe248d961669bdaeedecf696f503b7fc96ffe1d
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-18 09:19:32 -0700

    8-complex.c

---
+++ 0x00-math_complex/8-complex.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * complex_from_mod_arg - updates the real and the imaginary parts of a complex
+ * number given its modulus and arguments
+ * @m: modulus
+ * @arg: argument
+ * @c3: result
+ */
+
+void complex_from_mod_arg (double m, double arg, complex *c3)
+{
+	c3->re = tan(arg) * m;
+	c3->im = sin(arg) * m;
+}
